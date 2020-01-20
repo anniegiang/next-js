80413a75fb6db1d4865d23dd91462dcd01e6a355
Annie Giang <agiang@nisum.com>
2020-01-19 19:17:48 -0800

    Fetch API using getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-const Index = () => (
-  <div>
-    <h1>Index Page</h1>
-  </div>
-);
+import axios from "axios";
+
+const Index = props => {
+  return (
+    <div>
+      <h1>Index</h1>
+    </div>
+  );
+};
+
+Index.getInitialProps = async () => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  const data = res.data;
+
+  return { res: console.log(data) };
+};
 
 export default Index;
