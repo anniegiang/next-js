b11eb1fe08464a4277e6f307c3eb37242d1ccf49
Annie Giang <agiang@nisum.com>
2020-01-19 17:54:07 -0800

    Create CSR Link for Contact

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,10 @@ import Link from "next/link";
 const Index = () => (
   <div>
     <Link href="/about">
-      <a>About page</a>
+      <a>About Us</a>
+    </Link>
+    <Link href="/contact">
+      <a>Contact Us</a>
     </Link>
     <h1>Index Page</h1>
   </div>
