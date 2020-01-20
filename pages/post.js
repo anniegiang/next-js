8ab9a09caab8eb0b72dfbf61b8f123ee26ea1183
Annie Giang <agiang@nisum.com>
2020-01-19 19:48:23 -0800

    Render comments

--- pages/post.js
+++ pages/post.js
@@ -1,9 +1,25 @@
-const Post = props => {
-  return <h3>Post #{props.id}</h3>;
+import axios from "axios";
+
+const Post = ({ id, comments }) => {
+  return (
+    <div>
+      <h3>Post #{id}</h3>
+      <h2>Comments</h2>
+      <ul>
+        {comments.map(comment => (
+          <li>{comment.body}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 Post.getInitialProps = async ({ query }) => {
-  return { id: query.id };
+  const res = await axios.get(
+    `https://jsonplaceholder.typicode.com/comments?postId=${query.id}`
+  );
+  const data = res.data;
+  return { comments: data, id: query.id };
 };
 
 export default Post;
